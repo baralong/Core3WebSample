e7a210f9e65f3e80a7220415d4ce7500fb585efc
Doug Paice <doug.paice@baralong.org>
2019-05-07 13:36:54 +0800

    Add project files.

---
+++ Core3WebSample/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace Core3WebSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string SelectedShip { get; set; }
+        private readonly string[] ships =
+            {
+                "USS Adelphi",
+                "USS Enterprise",
+                "USS Excalibur",
+                "USS Exeter",
+                "USS Gandhi",
+                "USS Horatio",
+                "USS Valdemar",
+                "USS Yamaguchi",
+                "USS Zhukov",
+                "USS Loma Prieta",
+            };
+        public void OnGet()
+        {
+            var rand = new Random();
+            // loop 
+            foreach(var ship in ships)
+            {
+                if (rand.Next(0, 5) == 3)
+                {
+                    SelectedShip = ship;
+                    return;
+                }
+                SelectedShip = "";
+            }
+        }
+    }
+}
